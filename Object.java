f91c4fc05c2457c0b0323e937d787eb90561210e
Aravind S <91778740+Arvnd147@users.noreply.github.com>
2023-07-18 22:38:18 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,9 @@
+class Object{
+    public void test(){
+        System.out.println("Test Method");
+    }
+    public static void main(String[] args){
+        Object a = new Object();
+        a.test();
+    }
+}
